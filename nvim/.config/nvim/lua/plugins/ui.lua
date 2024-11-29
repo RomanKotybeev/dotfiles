f9cad3b05816c0012b934660b430338b3ff749f5
Roman Kotyubeev <favorshlitex at gmail dot com>
2024-11-29 20:37:56 +0500

    Configure nvim Telescope

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -12,6 +12,28 @@ return {
       { "<leader>gc", "<cmd>Telescope git_commits<CR>", mode = {"n"}, desc = "Telescope Git commits" },
     },
     cmd = "Telescope",
+    config = function(_, opts)
+      require('telescope').setup({
+        defaults = {
+          prompt_prefix = "   ",
+          selection_caret = " ",
+          entry_prefix = " ",
+          sorting_strategy = "ascending",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.55,
+            },
+            width = 0.87,
+            height = 0.80,
+          },
+          mappings = {
+            n = { ["q"] = require("telescope.actions").close },
+          },
+        },
+        extensions_list = { "themes", "terms" },
+      })
+    end
   },
   -- =========================== END ==============================
   -- ======================== TREESITTER ==========================
@@ -148,7 +170,7 @@ return {
     config = function()
       local default_opts = { noremap = true, silent = true }
       local function map(mode, lhs, rhs, desc)
-	vim.api.nvim_set_keymap(mode, lhs, rhs, vim.tbl_extend("force", default_opts, { desc = desc }))
+        vim.api.nvim_set_keymap(mode, lhs, rhs, vim.tbl_extend("force", default_opts, { desc = desc }))
       end
       map("n", "<A-j>", "<Cmd>BufferPrevious<CR>", "Bar previous")
       map("n", "<A-k>", "<Cmd>BufferNext<CR>", "Bar next")
